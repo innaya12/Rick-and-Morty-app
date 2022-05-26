64dbb06f44528811fd927797cb8b9c3f880d9037
innaya12 <yakubov.inna2@gmail.com>
2022-05-26 16:06:49 +0300

    homepage diaplays

---
+++ morty/src/components/homePage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const HomePage = () => {
+
+    
+    return (
+        <div>
+            <h1>Hello rickkk</h1>
+        </div>
+    )
+};
+
+export default HomePage;
